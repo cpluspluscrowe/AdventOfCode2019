98278e904ae6d37ca3e3e4a26447cf3360786d4e
Chad Crowe <ccrowe@ccrowe-mn1.linkedin.biz>
2019-12-01 12:15:50 -0800

    completed part 2 of day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,10 +1,13 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error};
+use std::cmp;
 
 fn main() -> Result<(), Error> {
     let path = "src/puzzle_input.txt";
-    let summation = get_mass_summation(path);
-    println!("The summation = {}", summation);
+    let old = get_mass_summation(path);
+    let summation = get_resursive_mass_summation(path);
+    println!("The part1 solution is = {}", old);
+    println!("The part2 solution = {}", summation);
     Ok(())
 }
 
@@ -18,10 +21,29 @@ fn get_mass_summation(path: &str) -> i32 {
     summation
 }
 
+fn get_resursive_mass_summation(path: &str) -> i32 {
+    let lines = get_file_lines(path);
+    let mut summation: i32 = 0;
+    for line in lines {
+        let line: i32 = line.unwrap().parse().unwrap();
+        summation += calculate_recursive_fuel_required(line);
+    }
+    summation
+}
+
 fn get_file_lines(path: &str) -> std::io::Lines<std::io::BufReader<std::fs::File>> {
     let input = File::open(path).unwrap();
     let buffered = BufReader::new(input);
     buffered.lines()
+}
+
+fn calculate_recursive_fuel_required(x: i32) -> i32 {
+    if x <= 0 {
+        return 0;
+    } else {
+        let mass = cmp::max(calculate_fuel_required(x), 0);
+        return mass + calculate_recursive_fuel_required(mass);
+    }
 }
 
 fn calculate_fuel_required(x: i32) -> i32 {
@@ -52,4 +74,22 @@ fn example4() {
 fn summation() {
     let path = "src/example_input.txt";
     assert_eq!(get_mass_summation(path), 34241);
+}
+
+#[test]
+fn recursive_summation_example1() {
+    let _path = "src/example_input.txt";
+    assert_eq!(calculate_recursive_fuel_required(14), 2);
+}
+
+#[test]
+fn recursive_summation_example2() {
+    let _path = "src/example_input.txt";
+    assert_eq!(calculate_recursive_fuel_required(1969), 966);
+}
+
+#[test]
+fn recursive_summation_example3() {
+    let _path = "src/example_input.txt";
+    assert_eq!(calculate_recursive_fuel_required(100756), 50346);
 }
