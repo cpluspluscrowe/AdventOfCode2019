dd59f7437d5d2d9a77b688ba51e9088bce22d373
Chad Crowe <ccrowe@ccrowe-mn1.linkedin.biz>
2019-12-02 15:19:33 -0800

    Added vector copying to day2 code

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,15 +4,22 @@ use fileio::get_array_of_numbers_input;
 fn main() -> Result<(), Error> {
     let path = "src/example_input.txt";
     let lines = get_array_of_numbers_input(path);
-    let input = &lines[0];
-    print_data(input);
+    let input = lines[0].to_vec();
+    run_updates(input);
     println!("Hello World");
     Ok(())
 }
 
-pub fn print_data(input: &Vec<i32>){
+pub fn run_updates(mut input: Vec<i32>) -> Vec<i32> {
+    input[0] = 5;
+    input
+}
+
+
+
+/*pub fn print_data(input: &Vec<i32>){
     for value in input {
         println!("{}",value);
     }
-}
+}*/
 
